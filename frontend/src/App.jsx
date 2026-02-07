5c62435fa58ab3da42113067cbb43fa6e469dc15
Gavin Pierce <piercegavin3@gmail.com>
2026-02-07 17:34:04 -0500

    Updated App.jsx with new routes and made it more organized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,33 @@ import Notifications from "./pages/Notifications";
 import ManageProfile from "./pages/ManageProfile";
 import Modules from "./pages/Modules";
 import Account from "./pages/Account";
+import CourseSelection from "./pages/CourseSelection";
+import CourseDisplay from "./pages/CourseDisplay";
+import CourseMaterial from "./pages/CourseMaterial";
+import MemberProfile from "./pages/MemberProfile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Auth */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
+        {/* Member area */}
         <Route path="/member/dashboard" element={<MemberDashboard />} />
         <Route path="/member/notifications" element={<Notifications />} />
-        <Route path="/member/profile" element={<ManageProfile />} />
+        <Route path="/member/profile" element={<MemberProfile />} />
+        <Route path="/member/manage-profile" element={<ManageProfile />} />
         <Route path="/member/modules" element={<Modules />} />
         <Route path="/member/account" element={<Account />} />
+
+        {/* Courses */}
+        <Route path="/member/courses" element={<CourseSelection />} />
+        <Route path="/member/course/:id" element={<CourseDisplay />} />
+        <Route path="/member/material/:id" element={<CourseMaterial />} />
       </Routes>
     </BrowserRouter>
   );
