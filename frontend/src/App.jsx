63e0d43a62f1c1aa2187f586a54f6fa1bdade97d
EthanBeam <beame2@mailbox.winthrop.edu>
2026-03-29 16:21:35 -0400

    Fixed App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,6 @@ import AdminProfile from "./pages/AdminProfile";
 import InstructorCourses from "./pages/InstructorCourses";
 import InstructorCourseDetail from "./pages/InstructorCourseDetail";
 import InstructorProfile from "./pages/InstructorProfile";
-import InstructorNotificationCenter from "./pages/InstructorNotificationCenter";
 import AdminFeedback from "./pages/AdminFeedback";
 import AdminReports from "./pages/AdminReports";
 
@@ -57,7 +56,6 @@ export default function App() {
         <Route path="/instructor/courses" element={<InstructorCourses />} />
         <Route path="/instructor/courses/:courseId" element={<InstructorCourseDetail />} />
         <Route path="/instructor/profile" element={<InstructorProfile />} />
-        <Route path="/instructor/notificationcenter" element={<InstructorNotificationCenter />} />
         <Route path="/admin/feedback" element={<AdminFeedback />} />
         <Route path="/admin/reports" element={<AdminReports />} />
       </Routes>
